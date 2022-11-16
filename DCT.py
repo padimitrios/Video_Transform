17c0c62a4e733ea84f43d354a5927fe591f23e32
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2022-11-16 14:58:31 +0200

    automate the dct dst generation array

--- DCT.py
+++ DCT.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math
 import yuvio
-import math
 
+SIZE = 4
 
 yuv_frame = yuvio.mimread("C:/Users/Dimitris/Desktop/Beauty_1920x1080_120fps_420_8bit_YUV.yuv", 1920, 1080, "yuv420p")
 
@@ -32,50 +32,30 @@ def DCT(A,X):
     A = np.transpose(A)
     #print(np.dot(Y,A))
     return np.dot(Y,A)
+
+#generates the array for operations for each transform
+def generate_DST_or_DCT(flag,n):
     
- 
+    arr = []
+    if flag :
+        
+        for i in range(n):
+            for j in range(n):
+                if i == 0:
+                  P = 1
+                else:
+                  P = 2**0.5
+              
+                arr.append(P/(n**0.5)*math.cos((math.pi)/n*(j + 0.5)*i))
+    else:
+        for i in range(n):
+            for j in range(n):
+                arr.append(round(128*(2/((2*n + 1)**0.5))*math.sin((2*i + 1)*(j + 1)*math.pi)/(2*n + 1)))
 
-# def dctTransform(matrix):
-# m = 4
-# n = 4
-#     # dct will store the discrete cosine transform
-#     dct = []
-#     for i in range(m):
-#         dct.append([None for _ in range(n)])
- 
-#     for i in range(m):
-#         for j in range(n):
- 
-#             # ci and cj depends on frequency as well as
-#             # number of row and columns of specified matrix
-#             if (i == 0):
-#                 ci = 1 / (m ** 0.5)
-#             else:
-#                 ci = (2 / m) ** 0.5
-#             if (j == 0):
-#                 cj = 1 / (n ** 0.5)
-#             else:
-#                 cj = (2 / n) ** 0.5
- 
-#             # sum will temporarily store the sum of
-#             # cosine signals
-#             sum = 0
-#             for k in range(m):
-#                 for l in range(n):
- 
-#                     dct1 = matrix[k][l] * math.cos((2 * k + 1) * i * pi / (
-#                         2 * m)) * math.cos((2 * l + 1) * j * pi / (2 * n))
-#                     sum = sum + dct1
- 
-#             dct[i][j] = ci * cj * sum
- 
-#     for i in range(m):
-#         for j in range(n):
-#             print(dct[i][j], end="\t")
-#         print()
+    
+    return np.array(arr).reshape((SIZE, -1))
 
-
-#matrix = [[5,11,8,10],[9,8,4,12],[1,10,11,4],[19,6,15,7]]
+dct_arr = generate_DST_or_DCT(1,SIZE)
 
 #take first frame of y
 c = yuv_frame[0].y
