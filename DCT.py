4637d184ea0d20a30cd9b5f1695903e9e19e00e5
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2022-11-18 21:10:48 +0200

    Changed transofrm matrix and normalized the values

--- DCT.py
+++ DCT.py
@@ -6,9 +6,6 @@ SIZE = 4
 
 yuv_frame = yuvio.mimread("C:/Users/Dimitris/Desktop/Beauty_1920x1080_120fps_420_8bit_YUV.yuv", 1920, 1080, "yuv420p")
 
-#print(yuv_frame[0].y)
-
-
 #https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
 def blockshaped(arr, nrows, ncols):
     """
@@ -25,77 +22,87 @@ def blockshaped(arr, nrows, ncols):
                .swapaxes(1,2)
                .reshape(-1, nrows, ncols))
 
-
-
-def DCT(A,X):
+def transform(A,X):
     Y = np.dot(A,X)
     A = np.transpose(A)
-    #print(np.dot(Y,A))
     return np.dot(Y,A)
 
-#generates the array for operations for each transform
-def generate_DST_or_DCT(flag,n):
-    
-    arr = []
-    if flag :
-        
-        for i in range(n):
-            for j in range(n):
-                if i == 0:
-                  P = 1
-                else:
-                  P = 2**0.5
-              
-                arr.append(P/(n**0.5)*math.cos((math.pi)/n*(j + 0.5)*i))
-    else:
-        for i in range(n):
-            for j in range(n):
-                arr.append(round(128*(2/((2*n + 1)**0.5))*math.sin((2*i + 1)*(j + 1)*math.pi)/(2*n + 1)))
+#fixed DCT HVEC array
+a_0 = 64
+b_0 = 83
+b_1 = 36
+dct_arr = [[a_0,a_0,a_0,a_0],
+           [b_0,b_1,b_1*(-1),b_0*(-1)],
+           [a_0,a_0*(-1),a_0*(-1),a_0],
+           [b_1,b_0*(-1),b_0,b_1*(-1)]]
 
-    
-    return np.array(arr).reshape((SIZE, -1))
-
-dct_arr = generate_DST_or_DCT(1,SIZE)
+#fixed DST HVEC array
+dst_arr = [[29,55,74,84],
+           [74,74,0,-74],
+           [84,-29,-74,55],
+           [55,-84,74,-29]]
 
 #take first frame of y
-c = yuv_frame[0].y
+first_y_frame = yuv_frame[0].y
+    
 #reform it into 4x4 lists
-c = blockshaped(c, 4, 4)
+first_y_frame = blockshaped(first_y_frame, SIZE, SIZE)
 
-#dct transform on the patrix
+#dct transform on the matrix
 transformed_matrix = []
-
-for frame in c:
-    transformed_matrix.append(DCT(dct_arr,frame))
+for frame in first_y_frame:
+    transformed_matrix.append(transform(dct_arr,frame))
 
 #calculate energy
-energy = [[0] * 4 for _ in range(4)]
+avg_energy = [[0] * SIZE for _ in range(SIZE)]
+avg_energy = np.array(avg_energy, dtype=np.int64)
+
 for frame in transformed_matrix:
     for i in range(len(frame)):
         for j in range(len(frame[0])):
-            energy[i][j] += (abs(frame[i][j])**2)
+            avg_energy[i][j] += (frame[i][j]//128)**2
 
-#calculate avergae energy
-avg_energy = []
-for val in energy:
-    for v in val:
-        avg_energy.append(v//len(c))
+for i in range(len(frame)):
+    for j in range(len(frame[0])):
+        avg_energy[i][j] = avg_energy[i][j]//len(first_y_frame)
 
 #reform array to 4x4
-avg_energy = np.array(avg_energy).reshape((4, -1))
+avg_energy = np.array(avg_energy).reshape((SIZE, -1))
      
 #calculate energy   
-initial_energy = [[0] * 4 for _ in range(4)]
-for frame in c:
+initial_avg_energy = [[0] * SIZE for _ in range(SIZE)]
+initial_avg_energy = np.array(initial_avg_energy, dtype=np.int64)
+
+for frame in first_y_frame:
     for i in range(len(frame)):
         for j in range(len(frame[0])):
-            initial_energy[i][j] += (abs(frame[i][j])**2)
+            initial_avg_energy[i][j] += frame[i][j]**2
 
-#calculate avergae energy
-initial_avg_energy = []
-for val in initial_energy:
-    for v in val:
-        initial_avg_energy.append(v//len(c))
+for i in range(len(frame)):
+    for j in range(len(frame[0])):
+        initial_avg_energy[i][j] = initial_avg_energy[i][j]//len(first_y_frame)
 
 #reform array to 4x4
-initial_avg_energy = np.array(initial_avg_energy).reshape((4, -1))
+initial_avg_energy = np.array(initial_avg_energy).reshape((SIZE, -1))
+
+#dst transform on the patrix
+transformed_matrix_dst = []
+for frame in first_y_frame:
+    transformed_matrix_dst.append(transform(dst_arr,frame))
+
+#calculate energy
+avg_energy_dst = [[0] * SIZE for _ in range(SIZE)]
+avg_energy_dst = np.array(avg_energy_dst, dtype=np.int64)
+
+for frame in transformed_matrix_dst:
+    for i in range(len(frame)):
+        for j in range(len(frame[0])):
+            avg_energy_dst[i][j] += (frame[i][j]//128)**2
+
+#calculate avergae energy
+for i in range(len(frame)):
+    for j in range(len(frame[0])):
+        avg_energy_dst[i][j] = avg_energy_dst[i][j]//len(first_y_frame)
+
+#reform array to 4x4
+avg_energy_dst = np.array(avg_energy_dst).reshape((SIZE, -1))
